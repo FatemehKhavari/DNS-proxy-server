ad4d4239a0100cab70e34b84116b5d35ecce143c
MahshidZeineddin <mahshidz2002@gmail.com>
2023-06-29 00:17:52 +0330

    main function created and two new function create for check and save in cache

--- project.py
+++ project.py
@@ -1,29 +1,33 @@
 import socket
 import json
 import socket
+import threading
+import redis
 
 CONFIG_FILE = 'config.json'  
 CACHE_EXPIRATION = 60 
 EXTERNAL_DNS_SERVERS = ['1.1.1.1','8.8.8.8', '8.8.4.4'] 
 
-HOST = 'localhost'
-PORT = 53
 
-with open('config.json') as f:
-    config = json.load(f)
+cache = redis.Redis(host='localhost', port=6379, db=0)
+
+def check_cache(domain):
+    return cache.get(domain)
+
+def save_cache(domain, ip):
+    cache.set(domain, ip, ex=CACHE_EXPIRATION)
+
+
+if __name__ == '__main__':
+    with open('config.json') as f:
+        config = json.load(f)
 
     dns_servers = config['servers-dns-external']
 
-with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as sock:
-    sock.bind((HOST, PORT))
-    print(f"Listening on {HOST}:{PORT}")
-    sock.settimeout(10)
-    while True:
-        try:
-            data, addr = sock.recvfrom(1024)
-            print(f"Received request from {addr[0]}:{addr[1]}")
-            response = b'\x81\x80\x00\x01\x00\x01\x00\x00\x00\x00\x00\x00\x06google\x03com\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00\x00\x00\x5e\x00\x04\xac\xd9\x00\x0c'
-            sock.sendto(response, addr)
-        except socket.timeout:
-            print("No data received within the timeout period. Stopping the program.")
-            break
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.bind(('0.0.0.0', 35853))
+    print("DNS Proxy Server is running.")
+
+
+    received_data, addr = server_socket.recvfrom(1024)
+    threading.Thread(target=process_dns_request, args=(received_data, addr)).start()  
