03d04fd970f425d1484d401c545e012fa0b4bf85
MahshidZeineddin <mahshidz2002@gmail.com>
2023-06-29 00:53:12 +0330

    a simple function to search domain in dns made it make a conection with dns server and send request and receive a response and made ip from that response and return that ip

--- project.py
+++ project.py
@@ -16,6 +16,25 @@ def check_cache(domain):
 def save_cache(domain, ip):
     cache.set(domain, ip, ex=CACHE_EXPIRATION)
 
+def send_dns_request(domain,dns_query_type):
+    response = None
+    for dns_server in dns_servers:
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            sock.settimeout(5)
+            sock.connect((dns_server, 53))
+            sock.sendto(packed_data, (dns_server, 53))
+            print(domain)
+            ip = socket.gethostbyname(domain)
+            response=ip
+            return response
+        except socket.timeout:
+            print(f"Request to {dns_server} timed out.")
+        finally:
+            if sock:
+                sock.close()
+    
+
 def process_dns_request(domain, client_address):
     
     print(f"Received DNS request for domain: {domain}")
